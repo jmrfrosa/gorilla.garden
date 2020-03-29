fd0b849fb0374521d5ee83880c66aa1f42ad86a1
João Rosa <jmrfrosa92@gmail.com>
2020-03-29 17:55:14 +0100

    using axios for sane error handling

--- client/src/scripts/connector.js
+++ client/src/scripts/connector.js
@@ -1,26 +1,20 @@
+import axios from 'axios';
+
 const connector = {
   getGorillas: () => {
-    return fetch('/gorillas', {
+    return axios({
       method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    }).then(handleError)
-      .catch(error => console.log(error))
-  },
-  addGorilla: (gorilla) => {
-    return fetch('/gorillas/create', {
-      method: "POST",
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(gorilla)
-    }).then(handleError)
-      .catch(error => {
-      console.log(error);
+      url: '/gorillas'
     });
   },
-}
-
-function handleError(response) {
-  if (!response.ok) throw Error(response.statusText);
-  return response.json();
+  addGorilla: (gorilla) => {
+    return axios({
+      method: 'POST',
+      url: '/gorillas/create',
+      data: gorilla
+    });
+  },
+  handleError: (error) => (console.log(error))
 }
 
 export default connector;
